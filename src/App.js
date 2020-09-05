17f5eb6d1b5e415e3bc7c7eaf83f6e9c1c5dca18
KLSeung <kevin.skyulee@gmail.com>
2020-09-04 22:44:25 -0700

    added todo state and input state using useState hooks and added functionality to add todo once button is clicked

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['Do task 1', 'Do task 2', 'Do task 3']);
+  const [input, setInput] = useState('');
+
+  //Event to occur when button is clicked
+  const addTodo = (event) => {
+    setTodos([...todos, input]);
+  }
+
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      <input />
-      <button>Add Todo</button>
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <button onClick={addTodo}>Add Todo</button>
+
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
